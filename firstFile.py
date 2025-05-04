8805286a719d03bcf2565e4d91853652f57fb9d4
i8284 <109680222+i8284@users.noreply.github.com>
2025-05-04 13:04:44 +0300

    Create firstFile.py

---
+++ firstFile.py
@@ -0,0 +1,24 @@
+HELP = """
+help - напечатать справку по программе.
+add - добавить задачу в список (название задачи запрашиваем у пользователя).
+show - напечатать все добавленные задачи."""
+
+tasks = []
+
+run = True
+
+while run:
+    command = input("Введите команду: ")
+    if command == "help":
+        print(HELP)
+    elif command == "show":
+        print(tasks)
+    elif command == "add":
+        task = input("Введите название задачи: ")
+        tasks.append(task)
+        print("Задача добавлена")
+    elif command == "exit":
+        print("exit")
+        break
+
+print("До свидания!")
